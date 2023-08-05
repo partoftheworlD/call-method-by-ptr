383a26cf5afb6fbae29dd9833050540869873b63
partoftheworlD <partoftheworld@vivaldi.net>
2023-08-05 23:17:30 +0300

    Implementation of reinterpret_cast macro

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 use std::mem;
 
-// macro_rules! reinterpret_cast {
-//     ($from: ty, $to: ty, $ptr: ty ) => {
-//         unsafe {
-//             mem::transmute::<$from, $to>>($ptr)
-//         }
-//     };
-// }
+macro_rules! reinterpret_cast {
+    ($from:ty, $to:ty, $ptr:expr) => {
+        unsafe { mem::transmute::<$from, $to>($ptr) }
+    };
+}
+
 fn function_a(u: u32) -> u32 {
     println!("i'm external function {u:?}");
     0
@@ -17,9 +16,9 @@ struct Droppable {
 }
 
 impl Droppable {
-    fn new(function_a: &*const u64) -> Droppable {
+    fn new(function_a: *const u64) -> Droppable {
         println!("> Create {function_a:?}");
-        Droppable { ptr: *function_a }
+        Droppable { ptr: function_a }
     }
 }
 
@@ -34,9 +33,9 @@ fn main() {
     type Proto = extern "C" fn(u32) -> u32;
     type Ptr = *const u64;
     // Getting a pointer to a function
-    let ptr_function_a = Droppable::new(&(function_a as *const u64));
+    let ptr_function_a = Droppable::new(function_a as *const u64);
     // Cast pointer to a function prototype
-    let function = unsafe { mem::transmute::<Ptr, Proto>(ptr_function_a.ptr) };
+    let function = reinterpret_cast!(Ptr, Proto, ptr_function_a.ptr);
     // Calling a function by pointer
     function(1337);
 }
