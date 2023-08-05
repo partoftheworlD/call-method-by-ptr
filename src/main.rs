094aa34fcbf5755ce3b0030c8da01d478c4df0f0
partoftheworlD <partoftheworld@vivaldi.net>
2023-08-05 23:53:43 +0300

    Cleaned up code and added some comments

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,24 @@
 use std::mem;
 
+// Macro for cpp fans
 macro_rules! reinterpret_cast {
     ($from:ty, $to:ty, $ptr:expr) => {
         unsafe { mem::transmute::<$from, $to>($ptr) }
     };
 }
 
+// "External" function
 fn function_a(u: u32) -> u32 {
     println!("i'm external function {u:?}");
     0
 }
 
+// Structure that will record address of created function_a
 struct Droppable {
     ptr: *const u64,
 }
 
+// Implement Droppable::new to get address of function_a and write it to Droppable.ptr
 impl Droppable {
     fn new(function_a: *const u64) -> Droppable {
         println!("> Create {function_a:?}");
@@ -22,6 +26,8 @@ impl Droppable {
     }
 }
 
+// Overload Drop::drop by our own variant of the destructor for Droppable trait
+// to output a message about freeing a specific address
 impl Drop for Droppable {
     fn drop(&mut self) {
         println!("> Dropping {:?}", self.ptr);
@@ -32,10 +38,13 @@ fn main() {
     // Function prototype definition
     type Proto = extern "C" fn(u32) -> u32;
     type Ptr = *const u64;
-    // Getting a pointer to a function
-    let ptr_function_a = Droppable::new(function_a as *const u64);
+
+    // Getting function_a pointer while destructor is running
+    let ptr_function_a: Droppable = Droppable::new(function_a as *const u64);
+
     // Cast pointer to a function prototype
-    let function = reinterpret_cast!(Ptr, Proto, ptr_function_a.ptr);
+    let function: Proto = reinterpret_cast!(Ptr, Proto, ptr_function_a.ptr);
+
     // Calling a function by pointer
     function(1337);
 }
